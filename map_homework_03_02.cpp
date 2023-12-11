a7eb172158ec24dfe801b07892c33861bf280b82
Andrey <andrey.zalozhenkov@pikta.ru>
2023-12-11 16:15:31 +0400

    return void type and rename

--- map_homework_03_02.cpp
+++ map_homework_03_02.cpp
@@ -1,8 +1,8 @@
 ﻿#include <iostream>
 #include <future>
 #include <thread>
-#include <algorithm>
-#include <functional>
+//#include <algorithm>
+//#include <functional>
 
 class Sum
 {
@@ -10,29 +10,31 @@ public:
 	Sum() {};
 	Sum(int&& v) : sum(std::move(v)) {};
 	int sum{ 0 };
-	void operator()(const int& n) { sum += n; }
+	void operator()(const int& n){ sum += n; }
 	Sum operator+(const Sum& rsum) { return Sum(this->sum + rsum.sum); }
 };
 
-template<typename it, typename func, int size = 8>
-func async_sum(it begin, it end, func f)
+template<typename it, class func, int size = 16>
+void parallel_for_each(const it* begin, const it* end, func& f)
 {
 	size_t cur_sz = std::distance(begin, end);
 	if (cur_sz <= size)
 	{
-		return std::for_each(begin, end, f);
+		f = std::for_each(begin, end, f);
+		return;
 	}
 	auto mid = begin;
 	std::advance(mid, cur_sz / 2);
-	auto fut_res = std::async(async_sum<it, func>, begin, mid, f);
-	return fut_res.get() + async_sum(mid, end, f);
+	auto fut_res = std::async(parallel_for_each<it, func>, begin, mid, f);
+	parallel_for_each(mid, end, f);
+	fut_res.get();
  }
 
 int main()
 {
 	int arr[]{ 15,68,79,55,2,63,98,69,889,129,44,338,681,4,275 };
-
-	Sum s = async_sum(std::begin(arr), std::end(arr), Sum());
+	Sum s;
+	parallel_for_each(std::begin(arr), std::end(arr), s);
 
 	std::cout << "Summ array is: " << s.sum << std::endl;
 	return 0;
